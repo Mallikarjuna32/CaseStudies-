f922471e33355f16f6eb84a7956a6ed05a4e1660
Arjun_Greemus <mallikarjuna81426@gmail.com>
2024-10-23 06:08:45 +0530

    This line code is added in the new branch and merged into the main brnach

--- java.java
+++ java.java
@@ -1,5 +1,8 @@
-class Simple{
-    public static void main(String args[]){
-     System.out.println("Hello World Program in Java");
+
+class Simple {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World Program in Java");
+        System.out.println("This line code is added in the new branch and merged into the main brnach");
     }
 }
